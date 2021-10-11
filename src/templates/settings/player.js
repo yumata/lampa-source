509b42853f9c6ae73ae3ce78ee498014fd525749
Progresso <progresso.ptz@gmail.com>
2021-10-11 04:03:37 +0300

    subtitles toggleable stroke & backdrop, size options, handling {\anX} tags

--- src/templates/settings/player.js
+++ src/templates/settings/player.js
@@ -10,6 +10,28 @@ let html = `<div>
         <div class="settings-param__value"></div>
         <div class="settings-param__descr">Автоматически переключать на следующую серию при окончание текущей</div>
     </div>
+    
+    <div class="is--has_subs">
+        <div class="settings-param-title"><span>Субтитры</span></div>
+        
+        <div class="settings-param selector" data-type="toggle" data-name="subtitles_size">
+            <div class="settings-param__name">Размер</div>
+            <div class="settings-param__value"></div>
+            <div class="settings-param__descr"></div>
+        </div>
+        
+        <div class="settings-param selector" data-type="toggle" data-name="subtitles_stroke">
+            <div class="settings-param__name">Использовать окантовку</div>
+            <div class="settings-param__value"></div>
+            <div class="settings-param__descr">Субтитры будут обведены черным цветом для улучшения читаемости</div>
+        </div>
+        
+        <div class="settings-param selector" data-type="toggle" data-name="subtitles_backdrop">
+            <div class="settings-param__name">Использовать подложку</div>
+            <div class="settings-param__value"></div>
+            <div class="settings-param__descr">Субтитры будут отображаться на полупрозрачной подложке для улучшения читаемости</div>
+        </div>
+    </div>  
 </div>`
 
 export default html
