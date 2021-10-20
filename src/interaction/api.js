461cf87230158daa5eab635c099154e51b355e20
Yamata <yumatakunsan@gmail.com>
2021-10-20 21:10:39 +0300

    Fix poster

--- src/interaction/api.js
+++ src/interaction/api.js
@@ -430,7 +430,7 @@ function collections(params, oncomplite, onerror){
                         item.original_title = element.originalName
                         item.release_date   = '0000'
                         item.vote_average   = 0
-                        item.poster         = element.basicCovers && element.basicCovers.items.length ? findPoster(element.basicCovers.items) + '?width=300&scale=1&quality=80&mediaType=jpeg' : ''
+                        item.poster         = element.basicCovers && element.basicCovers.items.length ? (findPoster(element.basicCovers.items) || element.basicCovers.items[0].url) + '?width=300&scale=1&quality=80&mediaType=jpeg' : ''
                     }
 
                     items.push(item)
