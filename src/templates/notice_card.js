74f92d3b6b6e5d9af52097359c0e0606a1f4bd70
Yamata <yumatakunsan@gmail.com>
2022-01-03 15:21:47 +0200

    Notice

---
+++ src/templates/notice_card.js
@@ -0,0 +1,17 @@
+let html = `<div class="notice notice--card selector">
+    <div class="notice__left">
+        <div class="notice__img">
+            <img src="{img}" />
+        </div>
+    </div>
+    <div class="notice__body">
+        <div class="notice__head">
+            <div class="notice__title">{title}</div>
+            <div class="notice__time">{time}</div>
+        </div>
+        
+        <div class="notice__descr">{descr}</div>
+    </div>
+</div>`
+
+export default html
