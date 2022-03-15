251d8c69f63a30beca47650e5aae51db37c7e7c7
Pytin Xyilo <91452496+yumata@users.noreply.github.com>
2022-03-15 16:34:34 +0200

    Webos add params for subtitles

---
+++ src/interaction/player/webos_subp.js
@@ -0,0 +1,141 @@
+import Keypad from '../keypad'
+import Storage from '../../utils/storage'
+import Arrays from '../../utils/arrays'
+
+let subparams
+
+let listener = function(e){
+    if(e.keycode == 405) getWebosmediaId(setSubtitleColor)
+    if(e.keycode == 406) getWebosmediaId(setSubtitleBackgroundColor)
+    if(e.keycode == 403) getWebosmediaId(setSubtitleFontSize)
+    if(e.keycode == 404) getWebosmediaId(setSubtitlePosition)
+}
+
+Keypad.listener.follow('keydown', listener)
+
+
+function luna(params, call, fail){
+    if(call) params.onSuccess = call
+
+    params.onFailure = (result)=>{
+        console.log('WebOS',params.method + " [fail][" + result.errorCode + "] " + result.errorText )
+
+        if(fail) fail()
+    }
+
+    console.log(params)
+
+    webOS.service.request("luna://com.webos.media", params)
+}
+
+function initStorage(){
+    if(!subparams){
+        subparams = Storage.get('webos_subs_params','{}')
+
+        Arrays.extend(subparams,{
+            color: 2,
+            font_size: 1,
+            bg_color: 'black',
+            position: -1
+        })
+    }
+}
+
+function subCallParams(mediaId, method, func_params){
+    initStorage()
+
+    let parameters = { 
+        mediaId
+    }
+
+    Arrays.extend(parameters, func_params)
+
+    luna({
+        parameters,
+        method
+    })
+
+    Storage.set('webos_subs_params', subparams)
+}
+
+function getWebosmediaId(func) {
+    let video = document.querySelector('video')
+
+    if(video && video.mediaId){
+        setTimeout(subCallParams(video.mediaId, func.name, func()),300)
+    }
+}
+
+function setSubtitleColor(){
+    subparams.color++
+
+    if(subparams.color == 6) subparams.color = 0
+
+    return {
+        color: subparams.color
+    }
+}
+
+function setSubtitleBackgroundColor(){
+    let bgcolors = ['black','white','yellow','red','green','blue']
+    let ixcolors = bgcolors.indexOf(subparams.color)
+
+    ixcolors++
+
+    if(ixcolors == -1) ixcolors = 0
+
+    subparams.bg_color = bgcolors[ixcolors]
+
+    return {
+        bgColor: subparams.bg_color
+    }
+}
+
+function setSubtitleFontSize(){
+    subparams.font_size++
+
+    if(subparams.font_size == 5) subparams.font_size = 0
+
+    return {
+        fontSize: subparams.font_size
+    }
+}
+
+function setSubtitlePosition(){
+    subparams.position++
+
+    if(subparams.position == 5) subparams.position = -3
+
+    return {
+        position: subparams.position
+    }
+}
+
+
+function initialize(){
+    let video = document.querySelector('video')
+
+    if(video && video.mediaId){
+        initStorage()
+
+        let methods = ['setSubtitleColor','setSubtitleBackgroundColor','setSubtitleFontSize','setSubtitlePosition']
+
+        let parameters = { 
+            mediaId: video.mediaId
+        }
+    
+        Arrays.extend(parameters, subparams)
+
+        methods.forEach(method=>{
+            luna({
+                parameters,
+                method
+            })
+        })
+        
+    }
+}
+
+export default {
+    initialize
+}
