1923015652e160a15124a5a2591b41ef7021d937
Pytin Xyilo <91452496+yumata@users.noreply.github.com>
2022-03-19 11:05:03 +0200

    Start adaptive work

--- src/templates/files.js
+++ src/templates/files.js
@@ -4,8 +4,10 @@ let html = `<div class="files">
             <img src="{img}" class="full-start__img" />
         </div>
 
-        <div class="files__title">{title}</div>
-        <div class="files__title-original">{original_title}</div>
+        <div>
+            <div class="files__title">{title}</div>
+            <div class="files__title-original">{original_title}</div>
+        </div>
     </div>
     <div class="files__body">
         
