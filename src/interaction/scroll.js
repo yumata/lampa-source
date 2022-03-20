65aae77afbca38f2141198d19e38de909f91a119
Pytin Xyilo <91452496+yumata@users.noreply.github.com>
2022-03-20 16:40:41 +0200

    Adaptive settings

--- src/interaction/scroll.js
+++ src/interaction/scroll.js
@@ -59,7 +59,7 @@ function create(params = {}){
         let parent = $(e.target).parents('.scroll')
 
         drag.enable = html.is(parent[0])
-        drag.enable = true
+        //drag.enable = true
 
         clearInterval(drag.time)
         clearTimeout(drag.time_animate)
