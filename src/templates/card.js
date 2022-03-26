ed5286da7743613048ca75e49bfd13b07af3817d
Pytin Xyilo <91452496+yumata@users.noreply.github.com>
2022-03-26 13:12:04 +0200

    Fix card size

--- src/templates/card.js
+++ src/templates/card.js
@@ -1,18 +1,16 @@
 let html = `<div class="card selector">
     <div class="card__view">
         <img src="./img/img_load.svg" class="card__img" />
-    </div>
 
-    <div class="card__icons">
-        <div class="card__icons-inner">
-            
+        <div class="card__icons">
+            <div class="card__icons-inner">
+                
+            </div>
         </div>
     </div>
-    
+
     <div class="card__title">{title}</div>
     <div class="card__age">{release_year}</div>
-
-    
 </div>`
 
 export default html
