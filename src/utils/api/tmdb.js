4b1e01cc4bf7312b4170d955b1fe6df900334937
Yamata <yumatakunsan@gmail.com>
2022-04-13 17:22:14 +0300

    Add proxy

--- src/utils/api/tmdb.js
+++ src/utils/api/tmdb.js
@@ -7,7 +7,7 @@ import Favorite from '../../utils/favorite'
 import Recomends from '../../utils/recomend'
 import VideoQuality from '../video_quality'
 
-let baseurl   = Utils.protocol() + 'apitm.kulik.uz/3/'
+let baseurl   = Utils.protocol() + 'apitmdb.cub.watch/3/'
 
 let network   = new Reguest()
 let key       = '4ef0d7355d9ffb5151e987764708ce96'
