9975b07e06f951d41f0a1e90c6c789a8575af9e1
nikk gitanes <tsynik@gmail.com>
2022-04-19 04:40:32 +0300

    correct some descriptions

--- src/templates/settings/interface.js
+++ src/templates/settings/interface.js
@@ -38,7 +38,7 @@ let html = `<div>
     <div class="settings-param selector" data-type="toggle" data-name="card_views_type">
         <div class="settings-param__name">Тип просмотра карточек</div>
         <div class="settings-param__value"></div>
-        <div class="settings-param__descr">По мере скроллинга ленты, карточки будут подгружаться постепенно или загружаться все</div>
+        <div class="settings-param__descr">По мере скроллинга ленты, карточки будут подгружаться постепенно или загружаться все.</div>
     </div> 
 
 </div>`
