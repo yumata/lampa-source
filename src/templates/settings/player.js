36e62ff78e14e107c217a503bcca29e1ab7badbc
nikk gitanes <tsynik@gmail.com>
2022-04-21 04:29:30 +0300

    Еще несколько исправлений в настройках

--- src/templates/settings/player.js
+++ src/templates/settings/player.js
@@ -14,7 +14,7 @@ let html = `<div>
     <div class="settings-param selector" data-type="toggle" data-name="playlist_next">
         <div class="settings-param__name">Следующая серия</div>
         <div class="settings-param__value"></div>
-        <div class="settings-param__descr">Автоматически переключать на следующую серию при окончание текущей</div>
+        <div class="settings-param__descr">Автоматически переключать на следующую серию по окончании текущей</div>
     </div>
 
     <div class="settings-param selector" data-type="toggle" data-name="player_timecode">
