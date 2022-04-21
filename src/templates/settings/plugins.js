83bdb2bc965fddca549b389ee4315fa975134c0d
nikk gitanes <tsynik@gmail.com>
2022-04-21 04:29:30 +0300

    Еще несколько исправлений в настройках

--- src/templates/settings/plugins.js
+++ src/templates/settings/plugins.js
@@ -1,7 +1,7 @@
 let html = `<div>
-    <div class="settings-param selector" data-name="plugins" data-static="true" data-notice="Для работы плагина, необходимо перезагрузить приложение.">
+    <div class="settings-param selector" data-name="plugins" data-static="true" data-notice="Для применения плагина необходимо перезагрузить приложение">
         <div class="settings-param__name">Добавить плагин</div>
-        <div class="settings-param__descr">Для удаления плагина удерживайте кнопку OK или двойной клик OK</div>
+        <div class="settings-param__descr">Для удаления добавленного плагина удерживайте или нажмите дважды кнопку OK на нем</div>
     </div>
     <div class="settings-param selector" data-name="install" data-static="true">
         <div class="settings-param__name">Установить плагин</div>
