4092146652ed070a2e7ab712409ef8108dcdf6d2
nikk gitanes <tsynik@gmail.com>
2022-05-06 03:54:16 +0300

    update ui and fix wide cards with light css

--- src/templates/settings/plugins.js
+++ src/templates/settings/plugins.js
@@ -1,7 +1,7 @@
 let html = `<div>
     <div class="settings-param selector" data-name="plugins" data-static="true" data-notice="Для применения плагина необходимо перезагрузить приложение">
         <div class="settings-param__name">Добавить плагин</div>
-        <div class="settings-param__descr">Для удаления добавленного плагина удерживайте или нажмите дважды кнопку OK на нем</div>
+        <div class="settings-param__descr">Для удаления добавленного плагина удерживайте или нажмите дважды клавишу (OK) на нем</div>
     </div>
     <div class="settings-param selector" data-name="install" data-static="true">
         <div class="settings-param__name">Установить плагин</div>
