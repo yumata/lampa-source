d7ee7e784e500dca020ade9f4a5dd66c32a1a97a
Yamata <yumatakunsan@gmail.com>
2022-05-19 11:25:34 +0300

    Fix title for episodes

--- plugins/online/component.js
+++ plugins/online/component.js
@@ -531,9 +531,9 @@ function component(object){
      */
     this.start = function(first_select){
         if(first_select){
-            var last_views = scroll.render().find('.selector.video--stream').find('.torrent-item__viewed').parent().last()
+            let last_views = scroll.render().find('.selector.video--stream').find('.torrent-item__viewed').parent().last()
 
-            if (last_views.length) last = last_views.eq(0)[0]
+            if (object.movie.number_of_seasons && last_views.length) last = last_views.eq(0)[0]
             else last = scroll.render().find('.selector').eq(3)[0]
         }
 
