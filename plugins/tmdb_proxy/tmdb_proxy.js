22db1c2b022305973c10dc0e7cd9d9de27ae1ec7
Yamata <91452496+yumata@users.noreply.github.com>
2022-10-30 11:08:56 +0200

    Change domain for proxy

--- plugins/tmdb_proxy/tmdb_proxy.js
+++ plugins/tmdb_proxy/tmdb_proxy.js
@@ -1,22 +1,22 @@
 let tmdb_proxy = {
     name: 'TMDB Proxy',
-    version: '1.0.0',
+    version: '1.0.1',
     description: 'Проксирование постеров и API сайта TMDB',
 
-    path_image: 'http://imagetmdb.cub.watch',
-    path_api: 'http://apitmdb.cub.watch',
+    path_image: 'http://imagetmdb.com/',
+    path_api: 'http://apitmdb.cub.watch/3/',
 }
 
 Lampa.TMDB.image = function(url){
     let base  = Lampa.Utils.protocol() + 'image.tmdb.org/' + url
 
-    return Lampa.Storage.field('proxy_tmdb') ? 'http://imagetmdb.cub.watch/' + url : base
+    return Lampa.Storage.field('proxy_tmdb') ? tmdb_proxy.path_image + url : base
 }
 
 Lampa.TMDB.api = function(url){
     let base  = Lampa.Utils.protocol() + 'api.themoviedb.org/3/' + url
 
-    return Lampa.Storage.field('proxy_tmdb') ? 'http://apitmdb.cub.watch/3/' + url : base
+    return Lampa.Storage.field('proxy_tmdb') ? tmdb_proxy.path_api + url : base
 }
 
 Lampa.Settings.listener.follow('open', function (e) {
