794fb59cfd72aeb3b39a6e224defee731c3832c3
Yamata <91452496+yumata@users.noreply.github.com>
2022-11-20 11:26:40 +0200

    Add version

--- plugins/online_prestige/online_prestige.js
+++ plugins/online_prestige/online_prestige.js
@@ -4,9 +4,9 @@ import Online from './component'
 function startPlugin() {
     window.online_prestige = true
 
-    Lampa.Manifest.plugins = {
+    let manifest = {
         type: 'video',
-        version: '1.0.0',
+        version: '1.0.8',
         name: 'Онлайн - Prestige',
         description: 'Плагин для просмотра онлайн сериалов и фильмов',
         component: 'online_prestige',
@@ -33,6 +33,8 @@ function startPlugin() {
             })
         }
     }
+
+    Lampa.Manifest.plugins = manifest
 
     Lampa.Lang.add({
         online_watch: {
@@ -308,7 +310,7 @@ function startPlugin() {
     }
 
 
-    const button = `<div class="full-start__button selector view--online" data-subtitle="v1.54">
+    const button = `<div class="full-start__button selector view--online" data-subtitle="Prestige v${manifest.version}">
         <svg width="135" height="147" viewBox="0 0 135 147" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M121.5 96.8823C139.5 86.49 139.5 60.5092 121.5 50.1169L41.25 3.78454C23.25 -6.60776 0.750004 6.38265 0.750001 27.1673L0.75 51.9742C4.70314 35.7475 23.6209 26.8138 39.0547 35.7701L94.8534 68.1505C110.252 77.0864 111.909 97.8693 99.8725 109.369L121.5 96.8823Z" fill="currentColor"/>
             <path d="M63 84.9836C80.3333 94.991 80.3333 120.01 63 130.017L39.75 143.44C22.4167 153.448 0.749999 140.938 0.75 120.924L0.750001 94.0769C0.750002 74.0621 22.4167 61.5528 39.75 71.5602L63 84.9836Z" fill="currentColor"/>
