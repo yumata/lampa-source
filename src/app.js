1b4752a0237f47dfcc70d13d8be36d0b771b1288
Pytin Xyilo <91452496+yumata@users.noreply.github.com>
2023-02-21 15:39:31 +0200

    Fix hls parser

--- src/app.js
+++ src/app.js
@@ -550,8 +550,6 @@ function startApp(){
             Noty.show('God enabled')
 
             window.god_enabled = true
-
-            //$('body').append('<div style="position: fixed; left: 0; top: 0; right: 0; bottom: 0; z-index: 300"></div>')
         }
     })
 
