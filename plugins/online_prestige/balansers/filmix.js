519db6cdb01a4af3a91dd2bcbe2b80e50ec952b2
Pytin Xyilo <91452496+yumata@users.noreply.github.com>
2023-03-07 13:29:12 +0200

    Fix 720p ads

--- plugins/online_prestige/balansers/filmix.js
+++ plugins/online_prestige/balansers/filmix.js
@@ -21,7 +21,10 @@ function filmix(component, _object){
             max_qualitie: 480,
             is_max_qualitie: false
         }
-    } 
+    }
+    else{
+        if(window.filmix.max_qualitie == 720) window.filmix.max_qualitie = 480
+    }
 
     let dev_token = 'user_dev_apk=2.0.1&user_dev_id=&user_dev_name=Xiaomi&user_dev_os=11&user_dev_token='+token+'&user_dev_vendor=Xiaomi'
 
