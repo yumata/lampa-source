9be2f9122c91f2eb521280081de34f0671adb0ee
Pytin Xyilo <91452496+yumata@users.noreply.github.com>
2023-04-18 15:44:29 +0300

    Add mic

--- src/utils/manifest.js
+++ src/utils/manifest.js
@@ -1,8 +1,8 @@
 let object = {
     author: 'Yumata',
     github: 'https://github.com/yumata/lampa-source',
-    css_version: '2.3.0',
-    app_version: '1.8.3',
+    css_version: '2.3.1',
+    app_version: '1.8.4',
     cub_domain: 'cub.watch'
 }
 
