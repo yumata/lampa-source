34b860756fabbb5ed8a422004e1999400fcfc9bc
Pytin Xyilo <91452496+yumata@users.noreply.github.com>
2023-05-09 15:58:24 +0300

    Add logs and key for philipse

--- src/utils/manifest.js
+++ src/utils/manifest.js
@@ -1,8 +1,8 @@
 let object = {
     author: 'Yumata',
     github: 'https://github.com/yumata/lampa-source',
-    css_version: '2.3.4',
-    app_version: '1.8.7',
+    css_version: '2.3.5',
+    app_version: '1.8.8',
     cub_domain: 'cub.watch'
 }
 
