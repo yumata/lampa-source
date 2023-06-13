45b6434b82df56ac73589ae6dce8cd4f80cadff3
yumata <91452496+yumata@users.noreply.github.com>
2023-06-13 17:08:08 +0300

    Fix

--- src/interaction/keyboard_layers.js
+++ src/interaction/keyboard_layers.js
@@ -75,9 +75,8 @@ let layers = {
     },
     nums: {
         'default': [
-            '0 1 2 3 4',
-            '5 6 7 8 9',
-            '{ENTER}',
+            '0 1 2 3 4 {BKSP}',
+            '5 6 7 8 9 {ENTER}',
         ]
     }
 }
