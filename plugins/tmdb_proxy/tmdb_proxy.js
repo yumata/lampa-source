759be888c66730ecf86b1a1015b752ed49a68eec
yumata <91452496+yumata@users.noreply.github.com>
2023-06-28 19:28:06 +0300

    Update proxy

--- plugins/tmdb_proxy/tmdb_proxy.js
+++ plugins/tmdb_proxy/tmdb_proxy.js
@@ -3,14 +3,31 @@ let tmdb_proxy = {
     version: '1.0.1',
     description: 'Проксирование постеров и API сайта TMDB',
 
-    path_image: 'https://imagetmdb.com/',
+    path_image: '://imagetmdb.com/',
     path_api: 'http://apitmdb.cub.watch/3/',
 }
+
+Lampa.SettingsApi.addParam({
+    component: 'tmdb',
+    param: {
+        name: 'tmdb_protocol',
+        type: 'select',
+        values: {
+            http: 'HTTP',
+            https: 'HTTPS',
+        },
+        default: 'https'
+    },
+    field: {
+        name: Lampa.Lang.translate('torrent_error_step_3'),
+    },
+    onChange: ()=>{}
+})
 
 Lampa.TMDB.image = function(url){
     let base  = Lampa.Utils.protocol() + 'image.tmdb.org/' + url
 
-    return Lampa.Storage.field('proxy_tmdb') ? tmdb_proxy.path_image + url : base
+    return Lampa.Storage.field('proxy_tmdb') ? Lampa.Storage.field('tmdb_protocol') + tmdb_proxy.path_image + url : base
 }
 
 Lampa.TMDB.api = function(url){
@@ -24,3 +41,5 @@ Lampa.Settings.listener.follow('open', function (e) {
         e.body.find('[data-parent="proxy"]').remove()
     }
 })
+
+console.log('TMDB-Proxy','started, enabled:', Lampa.Storage.field('proxy_tmdb'))
