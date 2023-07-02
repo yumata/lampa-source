7fd0f2e9f92cd9e8fbd052b14d32fc15c35d6555
yumata <91452496+yumata@users.noreply.github.com>
2023-07-02 13:48:07 +0300

    Add view all

--- src/components/bookmarks.js
+++ src/components/bookmarks.js
@@ -5,6 +5,7 @@ import Arrays from '../utils/arrays'
 
 function component(object){
     let comp = new Lampa.InteractionMain(object)
+    let viev_all = false
 
     comp.create = function(){
         this.activity.loader(true)
@@ -33,14 +34,32 @@ function component(object){
         return this.render()
     }
 
-    comp.onMore = function(line){
-        Activity.push({
-            url: '',
-            title: Lang.translate('title_' + line.type),
-            component: 'favorite',
-            type: line.type,
-            page: 2
+    comp.onAppend = function(line){
+        line.render(true).on('visible',()=>{
+            let more = line.render(true).find('.items-line__more')
+
+            if(more){
+                more.text(Lang.translate('settings_param_card_view_all'))
+
+                more.on('hover:enter',()=>{
+                    viev_all = true
+                })
+            }
         })
+    }
+
+    comp.onMore = function(line){
+        setTimeout(()=>{
+            Activity.push({
+                url: '',
+                title: Lang.translate('title_' + line.type),
+                component: 'favorite',
+                type: line.type,
+                page: viev_all ? 1 : 2
+            })
+
+            viev_all = false
+        },50)
     }
 
     return comp
