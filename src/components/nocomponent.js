94cc0c84b404949d8af2142ece4fe3154990d8d8
yumata <91452496+yumata@users.noreply.github.com>
2023-11-15 11:32:47 +0200

    Restart component

--- src/components/nocomponent.js
+++ src/components/nocomponent.js
@@ -1,12 +1,40 @@
 import Controller from '../interaction/controller'
 import Empty from '../interaction/empty'
 import Activity from '../interaction/activity'
+import Lang from '../utils/lang'
 
 function component(object){
     let html = $('<div></div>')
     let empty = new Empty()
     
     this.create = function(){
+        let card = object.movie || object.card
+        let foot = $('<div class="empty__footer"></div>')
+
+        let button_reset = $('<div class="simple-button selector">'+ Lang.translate('title_reset') +'</div>')
+        let button_movie = $('<div class="simple-button selector">'+ Lang.translate('back_to_card') +'</div>')
+
+        button_reset.on('hover:enter',()=>{
+            Activity.replace()
+        })
+
+        foot.append(button_reset)
+
+        if(card){
+            button_movie.on('hover:enter',()=>{
+                Activity.replace({
+                    component: 'full',
+                    card: card,
+                    id: card.id,
+                    method: card.number_of_seasons ? 'tv' : 'movie',
+                    source: card.source || 'cub'
+                })
+            })
+
+            foot.append(button_movie)
+        }
+
+        empty.append(foot)
 
         html.append(empty.render())
 
