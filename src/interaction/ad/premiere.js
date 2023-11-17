78b0a110ceb0fcf3723c19409b3e6b362677309a
yumata <91452496+yumata@users.noreply.github.com>
2023-11-17 11:04:45 +0200

    Fixed premier text

--- src/interaction/ad/premiere.js
+++ src/interaction/ad/premiere.js
@@ -8,63 +8,6 @@ import Storage from '../../utils/storage'
 
 let network
 
-let translates = {
-    premiere_author_recomend_1: {
-        ru: 'Закажите пиццу и готовьтесь к драйву.',
-        en: 'Order pizza and get ready for some excitement.',
-        uk: 'Замовте піцу та підготуйтеся до драйву.',
-        be: 'Замовіце піцу і падрыхтуйцеся да драйву.',
-        pt: 'Peça pizza e prepare-se para a emoção.',
-        zh: '订披萨，准备好迎接刺激。',
-        bg: 'Поръчайте пица и се пригответе за забава',
-    },
-    premiere_author_recomend_2: {
-        ru: 'Вечеринка только начинается.',
-        en: 'The party is just getting started.',
-        uk: 'Вечірка тільки починається.',
-        be: 'Вечарынка толькі пачынаецца.',
-        pt: 'A festa está apenas começando.',
-        zh: '派对刚刚开始。',
-        bg: 'Вечеринката тъкмо започва'
-    },
-    premiere_author_recomend_3: {
-        ru: 'Подготовьтесь к адреналину.',
-        en: 'Get ready for some adrenaline.',
-        uk: 'Підготуйтесь до адреналіну.',
-        be: 'Прыгатуйцеся да адрэналіну.',
-        pt: 'Prepare-se para a adrenalina.',
-        zh: '准备迎接肾上腺素。',
-        bg: 'Бъдете готови за адреналина.',
-    },
-    premiere_author_recomend_4: {
-        ru: 'Готовы к незабываемому вечеру?',
-        en: 'Ready for an unforgettable evening?',
-        uk: 'Готові до незабутнього вечора?',
-        be: 'Гатовыя да нязабыўнага вечара?',
-        pt: 'Pronto para uma noite inesquecível?',
-        zh: '准备好度过难忘的夜晚了吗？',
-        bg: 'Готови ли сте за една незабравима вечер?',
-    },
-    premiere_author_recomend_5: {
-        ru: 'Рекомендую к просмотру.',
-        en: 'I recommend watching it.',
-        uk: 'Я рекомендую його подивитися.',
-        be: 'Я рэкамендую гэта праглядзець.',
-        pt: 'Eu recomendo assistir.',
-        zh: '我推荐观看它。',
-        bg: 'Препоръчвам ви да гледате',
-    },
-    premiere_title: {
-        ru: 'Премьера',
-        en: 'Premiere',
-        uk: 'Прем\'єра',
-        be: 'Прем’ера',
-        pt: 'Estréia',
-        zh: '首映',
-        bg: 'Премиера',
-    }
-}
-
 function init(){
     network = new Reguest()
 
@@ -102,7 +45,7 @@ function update(data){
             }
 
             movie.translations.translations.filter(t=>codes.indexOf(t.iso_639_1) >= 0).forEach(t=>{
-                notice.title[t.iso_639_1] = translates.premiere_title[t.iso_639_1] + ': ' + (t.data.title || t.data.name || movie.title || movie.name)
+                notice.title[t.iso_639_1] = Lang.translate('premiere_title') + ': ' + (t.data.title || t.data.name || movie.title || movie.name)
                 notice.text[t.iso_639_1]  = (t.data.overview || movie.overview || '').slice(0,130) + '...'
             })
 
@@ -116,13 +59,11 @@ function update(data){
                 if(casts.length){
                     notice.author = {}
 
-                    let recomend = translates['premiere_author_recomend_' + (Math.floor(Math.random() * 5) + 1)]
-
                     codes.forEach(c=>{
                         notice.author[c] = {
                             name: casts[0].character,
                             img: casts[0].profile_path,
-                            text: recomend[c] || recomend.en
+                            text: Lang.translate('premiere_author_recomend_' + (Math.floor(Math.random() * 5) + 1))
                         }
                     })
                 }
