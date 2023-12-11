277e1cd76909688dfa2e9d764632950fd69e72c5
yumata <91452496+yumata@users.noreply.github.com>
2023-12-11 17:43:57 +0200

    Fixed normalization visible

--- src/utils/manifest.js
+++ src/utils/manifest.js
@@ -1,8 +1,8 @@
 let object = {
     author: 'Yumata',
     github: 'https://github.com/yumata/lampa-source',
-    css_version: '2.6.7',
-    app_version: '2.0.2',
+    css_version: '2.6.8',
+    app_version: '2.0.3',
     cub_domain: 'cub.red'
 }
 
