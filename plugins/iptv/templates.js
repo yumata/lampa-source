91eba3ce481ef92bbc0c74e8b30a7b1c7df6be11
yumata <91452496+yumata@users.noreply.github.com>
2023-12-15 12:37:39 +0200

    Add about for IPTV

--- plugins/iptv/templates.js
+++ plugins/iptv/templates.js
@@ -204,6 +204,37 @@ function init(){
         </div>
     </div>`)
 
+    // if(window.lampa_settings.iptv){
+        Lampa.Template.add('about',`<div class="about">
+        <div>#{iptv_about_text}</div>
+    
+    
+        <div class="overhide">
+            <div class="about__contacts">
+                <div>
+                    <small>#{about_channel}</small><br>
+                    @lampa_channel
+                </div>
+    
+                <div>
+                    <small>#{about_group}</small><br>
+                    @lampa_group
+                </div>
+    
+                <div>
+                    <small>#{about_version}</small><br>
+                    <span class="version_app"></span>
+                </div>
+    
+                <div class="hide platform_android">
+                    <small>#{about_version} Android</small><br>
+                    <span class="version_android"></span>
+                </div>
+            </div>
+        </div>
+    </div>`)
+    // }
+
     Lampa.Template.add('cub_iptv_style', `
         <style>
         @@include('../plugins/iptv/css/style.css')
