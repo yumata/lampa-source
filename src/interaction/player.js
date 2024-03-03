85e82c7619fe82290e4598e5bee617aa1a15f201
yumata <91452496+yumata@users.noreply.github.com>
2024-03-03 18:14:54 +0200

    Add svplayer

--- src/interaction/player.js
+++ src/interaction/player.js
@@ -807,6 +807,7 @@ function iptv(data){
         else if(Platform.is('apple_tv')){
             if(Storage.field('player_iptv') == 'vlc')          window.location.assign('vlc-x-callback://x-callback-url/stream?url=' + encodeURIComponent(data.url))
             else if(Storage.field('player_iptv') == 'infuse')  window.location.assign('infuse://x-callback-url/play?url='+encodeURIComponent(data.url))
+            else if(Storage.field('player_iptv') == 'svplayer')window.location.assign('svplayer://x-callback-url/stream?url=' + encodeURIComponent(data.url))
             else if (Storage.field('player_iptv') == 'tvos')   window.location.assign('lampa://video?player=tvos&src=' + encodeURIComponent(data.url))
             else lauch()
         }
