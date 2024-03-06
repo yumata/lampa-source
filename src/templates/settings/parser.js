5881efe6bdb4ec81cf7e9a48b7644025f963e69c
Xinos <userxinos@gmail.com>
2024-03-06 21:45:14 +0700

    implementation api prowlarr

--- src/templates/settings/parser.js
+++ src/templates/settings/parser.js
@@ -5,7 +5,7 @@ let html = `<div>
         <div class="settings-param__descr">#{settings_parser_use_descr}</div>
     </div>
     <div data-parent="parser">
-        <div class="settings-param selector hide" data-type="toggle" data-name="parser_torrent_type" data-children="type" data-children-value="jackett">
+        <div class="settings-param selector" data-type="toggle" data-name="parser_torrent_type" data-children="type" data-children-value="jackett">
             <div class="settings-param__name">#{settings_parser_type}</div>
             <div class="settings-param__value"></div>
         </div>
@@ -27,6 +27,21 @@ let html = `<div>
         <div class="settings-param selector" data-type="toggle" data-name="jackett_interview" data-parent="type">
             <div class="settings-param__name">#{settings_parser_jackett_interview}</div>
             <div class="settings-param__value"></div>
+        </div>
+        
+
+        <div class="settings-param-title" data-parent="type-prowlarr"><span>Prowlarr</span></div>
+
+        <div class="settings-param selector" data-type="input" data-name="prowlarr_url" placeholder="#{settings_parser_jackett_placeholder}" data-parent="type-prowlarr">
+            <div class="settings-param__name">#{settings_parser_jackett_link}</div>
+            <div class="settings-param__value"></div>
+            <div class="settings-param__descr">#{settings_parser_prowlarr_link_descr}</div>
+        </div>
+
+        <div class="settings-param selector" data-type="input" data-name="prowlarr_key" data-string="true" placeholder="#{settings_parser_jackett_key_placeholder}" data-parent="type-prowlarr">
+            <div class="settings-param__name">#{settings_parser_jackett_key}</div>
+            <div class="settings-param__value"></div>
+            <div class="settings-param__descr">#{settings_parser_prowlarr_key_descr}</div>
         </div>
 
         <div class="settings-param-title"><span>#{more}</span></div>
