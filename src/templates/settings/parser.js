40a5a292d14c20fc4702910a3e87090118130808
yumata <91452496+yumata@users.noreply.github.com>
2024-03-09 13:14:53 +0200

    Fixed prowlarr

--- src/templates/settings/parser.js
+++ src/templates/settings/parser.js
@@ -5,43 +5,46 @@ let html = `<div>
         <div class="settings-param__descr">#{settings_parser_use_descr}</div>
     </div>
     <div data-parent="parser">
-        <div class="settings-param selector" data-type="toggle" data-name="parser_torrent_type" data-children="type" data-children-value="jackett">
+        <div class="settings-param selector" data-type="toggle" data-name="parser_torrent_type" data-children="type">
             <div class="settings-param__name">#{settings_parser_type}</div>
             <div class="settings-param__value"></div>
         </div>
+
+        <div data-parent="type" data-visible-value="jackett" class="hide">
+            <div class="settings-param-title"><span>Jackett</span></div>
+
+            <div class="settings-param selector" data-type="input" data-name="jackett_url" placeholder="#{settings_parser_jackett_placeholder}">
+                <div class="settings-param__name">#{settings_parser_jackett_link}</div>
+                <div class="settings-param__value"></div>
+                <div class="settings-param__descr">#{settings_parser_jackett_link_descr}</div>
+            </div>
+
+            <div class="settings-param selector" data-type="input" data-name="jackett_key" data-string="true" placeholder="#{settings_parser_jackett_key_placeholder}">
+                <div class="settings-param__name">#{settings_parser_jackett_key}</div>
+                <div class="settings-param__value"></div>
+                <div class="settings-param__descr">#{settings_parser_jackett_key_descr}</div>
+            </div>
+
+            <div class="settings-param selector" data-type="toggle" data-name="jackett_interview">
+                <div class="settings-param__name">#{settings_parser_jackett_interview}</div>
+                <div class="settings-param__value"></div>
+            </div>
+        </div>
         
-        <div class="settings-param-title" data-parent="type"><span>Jackett</span></div>
+        <div data-parent="type" data-visible-value="prowlarr" class="hide">
+            <div class="settings-param-title"><span>Prowlarr</span></div>
 
-        <div class="settings-param selector" data-type="input" data-name="jackett_url" placeholder="#{settings_parser_jackett_placeholder}" data-parent="type">
-            <div class="settings-param__name">#{settings_parser_jackett_link}</div>
-            <div class="settings-param__value"></div>
-            <div class="settings-param__descr">#{settings_parser_jackett_link_descr}</div>
-        </div>
+            <div class="settings-param selector" data-type="input" data-name="prowlarr_url" placeholder="#{settings_parser_jackett_placeholder}">
+                <div class="settings-param__name">#{settings_parser_jackett_link}</div>
+                <div class="settings-param__value"></div>
+                <div class="settings-param__descr">#{settings_parser_prowlarr_link_descr}</div>
+            </div>
 
-        <div class="settings-param selector" data-type="input" data-name="jackett_key" data-string="true" placeholder="#{settings_parser_jackett_key_placeholder}" data-parent="type">
-            <div class="settings-param__name">#{settings_parser_jackett_key}</div>
-            <div class="settings-param__value"></div>
-            <div class="settings-param__descr">#{settings_parser_jackett_key_descr}</div>
-        </div>
-
-        <div class="settings-param selector" data-type="toggle" data-name="jackett_interview" data-parent="type">
-            <div class="settings-param__name">#{settings_parser_jackett_interview}</div>
-            <div class="settings-param__value"></div>
-        </div>
-        
-
-        <div class="settings-param-title" data-parent="type-prowlarr"><span>Prowlarr</span></div>
-
-        <div class="settings-param selector" data-type="input" data-name="prowlarr_url" placeholder="#{settings_parser_jackett_placeholder}" data-parent="type-prowlarr">
-            <div class="settings-param__name">#{settings_parser_jackett_link}</div>
-            <div class="settings-param__value"></div>
-            <div class="settings-param__descr">#{settings_parser_prowlarr_link_descr}</div>
-        </div>
-
-        <div class="settings-param selector" data-type="input" data-name="prowlarr_key" data-string="true" placeholder="#{settings_parser_jackett_key_placeholder}" data-parent="type-prowlarr">
-            <div class="settings-param__name">#{settings_parser_jackett_key}</div>
-            <div class="settings-param__value"></div>
-            <div class="settings-param__descr">#{settings_parser_prowlarr_key_descr}</div>
+            <div class="settings-param selector" data-type="input" data-name="prowlarr_key" data-string="true" placeholder="#{settings_parser_jackett_key_placeholder}">
+                <div class="settings-param__name">#{settings_parser_jackett_key}</div>
+                <div class="settings-param__value"></div>
+                <div class="settings-param__descr">#{settings_parser_prowlarr_key_descr}</div>
+            </div>
         </div>
 
         <div class="settings-param-title"><span>#{more}</span></div>
