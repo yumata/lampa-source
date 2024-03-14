ab98d683aeca62bc7a5d8272d51e3cc6631278a2
yumata <91452496+yumata@users.noreply.github.com>
2024-03-14 14:32:55 +0200

    Home Page Components

--- src/components/main.js
+++ src/components/main.js
@@ -1,7 +1,18 @@
 import Api from '../interaction/api'
+import Manifest from '../utils/manifest'
 
 function component(object){
     let comp = new Lampa.InteractionMain(object)
+
+    comp.onLinesBuild = function(data){
+        Manifest.plugins.forEach(plugin=>{
+            if(plugin.onMain){
+                let result = plugin.onMain(data)
+                
+                if(result.results.length) comp.append(result)
+            }
+        })
+    }
 
     comp.create = function(){
         this.activity.loader(true)
