6456b2ffef7cbd3a17a7522aa90bb9ea39146b19
Seriy <sergejsivk@mail.ru>
2024-05-11 04:38:49 +0300

    Добавил тип парсера для торрентов TorrServer Чтобы работало в настройках TorrServer нужно включить поиск по RuTor

--- src/components/settings/params.js
+++ src/components/settings/params.js
@@ -433,7 +433,8 @@ select('poster_size',{
 
 select('parser_torrent_type',{
     'jackett': 'Jackett',
-    'prowlarr': 'Prowlarr'
+    'prowlarr': 'Prowlarr',
+    'torrserver': 'TorrServer'
 },'jackett')
 
 select('jackett_interview',{
