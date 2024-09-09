c90b5541bc002305b3c18f22dc2b1ce4b5b59f38
yumata <91452496+yumata@users.noreply.github.com>
2024-09-09 10:12:16 +0300

    Fix

--- plugins/tmdb_proxy/tmdb_proxy.js
+++ plugins/tmdb_proxy/tmdb_proxy.js
@@ -7,16 +7,23 @@ let tmdb_proxy = {
     path_api: 'apitmdb.'+(Lampa.Manifest && Lampa.Manifest.cub_domain ? Lampa.Manifest.cub_domain : 'cub.red')+'/3/'
 }
 
+function filter(u){
+    let s = u.slice(0, 8)
+    let e = u.slice(8).replace(/\/+/g,'/')
+
+    return s + e
+}
+
 Lampa.TMDB.image = function(url){
     let base  = Lampa.Utils.protocol() + 'image.tmdb.org/' + url
 
-    return Lampa.Storage.field('proxy_tmdb') ? Lampa.Utils.protocol() + tmdb_proxy.path_image + url : base
+    return filter(Lampa.Storage.field('proxy_tmdb') ? Lampa.Utils.protocol() + tmdb_proxy.path_image + url : base)
 }
 
 Lampa.TMDB.api = function(url){
     let base  = Lampa.Utils.protocol() + 'api.themoviedb.org/3/' + url
 
-    return Lampa.Storage.field('proxy_tmdb') ? Lampa.Utils.protocol() + tmdb_proxy.path_api + url : base
+    return filter(Lampa.Storage.field('proxy_tmdb') ? Lampa.Utils.protocol() + tmdb_proxy.path_api + url : base)
 }
 
 Lampa.Settings.listener.follow('open', function (e) {
