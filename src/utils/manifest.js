030f418bfccf6eaaff2d9e3fc4481ee0071ecbfc
yumata <91452496+yumata@users.noreply.github.com>
2024-10-22 16:21:03 +0300

    Fix

--- src/utils/manifest.js
+++ src/utils/manifest.js
@@ -1,7 +1,7 @@
 let object = {
     author: 'Yumata',
     github: 'https://github.com/yumata/lampa-source',
-    css_version: '2.9.3',
+    css_version: '2.9.4',
     app_version: '2.1.7',
     cub_domain: 'cub.red'
 }
