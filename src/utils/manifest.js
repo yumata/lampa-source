52d28b2288d6715cc9cb251c55359148a0b2df77
yumata <91452496+yumata@users.noreply.github.com>
2025-01-30 12:26:33 +0200

    Timings have been corrected

--- src/utils/manifest.js
+++ src/utils/manifest.js
@@ -2,7 +2,7 @@ let object = {
     author: 'Yumata',
     github: 'https://github.com/yumata/lampa-source',
     css_version: '2.6.1',
-    app_version: '2.2.7',
+    app_version: '2.2.8',
     cub_mirrors: ['cub.red', 'standby.cub.red']
 }
 
