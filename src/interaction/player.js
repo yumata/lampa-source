d5d59485732a7fda4fa4df0bd22a4a305ee1bbe7
Felix <midi2001@mail.ru>
2025-02-09 20:19:36 +0200

    back to lampa

--- src/interaction/player.js
+++ src/interaction/player.js
@@ -711,7 +711,7 @@ function start(data, need, inner){
         if(Storage.field(player_need) == 'vlc') window.location.assign('vlc-x-callback://x-callback-url/stream?url=' + encodeURIComponent(data.url))
         else if(Storage.field(player_need) == 'infuse') window.location.assign('infuse://x-callback-url/play?url='+encodeURIComponent(data.url))
         else if(Storage.field(player_need) == 'vidhub') window.open('open-vidhub://x-callback-url/open?url='+encodeURIComponent(data.url))
-        else if(Storage.field(player_need) == 'senplayer') window.open('SenPlayer://x-callback-url/play?url='+encodeURIComponent(data.url))
+        else if(Storage.field(player_need) == 'senplayer') window.open('SenPlayer://x-callback-url/play?on-success=lampa://&on-failed=lampa://&url='+encodeURIComponent(data.url))
         else if(Storage.field(player_need) == 'svplayer') window.location.assign('svplayer://x-callback-url/stream?url=' + encodeURIComponent(data.url))
         else if (Storage.field(player_need) == 'tvos') window.location.assign('lampa://video?player=tvos&src=' + encodeURIComponent(data.url) + '&playlist=' + encodeURIComponent(JSON.stringify(data.playlist)))
         else if (Storage.field(player_need) == 'tvosSelect') window.location.assign('lampa://video?player=lists&src=' + encodeURIComponent(data.url) + '&playlist=' + encodeURIComponent(JSON.stringify(data.playlist)))
