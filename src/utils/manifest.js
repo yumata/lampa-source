8d94bf5ba452d3f4b21e1e9bf4ee0b506496a1b8
yumata <91452496+yumata@users.noreply.github.com>
2025-04-15 16:20:54 +0300

    Fix vast load

--- src/utils/manifest.js
+++ src/utils/manifest.js
@@ -3,7 +3,7 @@ let object = {
     github: 'https://github.com/yumata/lampa-source',
     github_lampa: 'https://yumata.github.io/lampa/',
     css_version: '2.6.5',
-    app_version: '2.3.9',
+    app_version: '2.4.0',
     cub_site: 'cub.rip'
 }
 
