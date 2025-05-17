ae368f7cf2c2bee447c061c229444d89a4914ea5
yumata <91452496+yumata@users.noreply.github.com>
2025-05-17 15:00:20 +0300

    Cache for storage

--- src/utils/account/api.js
+++ src/utils/account/api.js
@@ -2,6 +2,7 @@ import Permit from './permit'
 import Utils from '../math'
 import Manifest from '../manifest'
 import Reguest from '../reguest'
+import Arrays from '../arrays'
 
 let network = new Reguest()
 
@@ -9,20 +10,22 @@ function url(){
     return Utils.protocol() + Manifest.cub_domain + '/api/'
 }
 
-function load(path){
+function load(path, params = {}){
     return new Promise((resolve, reject)=>{
         if(Permit.token){
             let account = Permit.account
 
-            network.silent(url() + path, resolve, (e)=>{
-                reject(network.errorCode(e))
-            },false,{
+            Arrays.extend(params, {
                 headers: {
                     token: account.token,
                     profile: account.profile.id
                 },
                 timeout: 8000
             })
+
+            network.silent(url() + path, resolve, (e)=>{
+                reject(network.errorCode(e))
+            }, false, params)
         }
         else{
             reject(403)
