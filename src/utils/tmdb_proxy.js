6cb3c7d50b3fa7f14e67d5a2034b009ce0b08391
yumata <91452496+yumata@users.noreply.github.com>
2025-06-21 12:03:37 +0300

    Add backup proxy

--- src/utils/tmdb_proxy.js
+++ src/utils/tmdb_proxy.js
@@ -26,6 +26,48 @@ function init(){
         return Storage.get('account','{}').email || ''
     }
 
+    function check(){
+        console.log('TMDB-Proxy', 'start check')
+
+        $.ajax({
+            url: TMDB.api('discover/movie?with_genres=14'),
+            dataType: 'json',
+            timeout: 6000,
+            error: function(){
+                console.log('TMDB-Proxy','api error', tmdb_proxy.path_api + ' not responding, using backup proxy')
+
+                tmdb_proxy.path_api = 'lampa.byskaz.ru/tmdb/api/3/'
+            }
+        })
+
+        $.ajax({
+            url: Utils.addUrlComponent(Utils.protocol() + 'tmdb.'+Manifest.cub_domain+'/?cat=movie&sort=latest&uhd=true', 'email=' + encodeURIComponent(email)),
+            dataType: 'json',
+            timeout: 6000,
+            error: function(){
+                console.log('TMDB-Proxy','tmdb error', 'tmdb.'+Manifest.cub_domain + ' not responding, using backup proxy')
+
+                Lampa.Listener.follow('request_before', function(e) {
+                    e.params.url = e.params.url.replace('tmdb.' + Manifest.cub_domain, 'tmdb.lampadev.ru')
+                })
+            }
+        })
+
+        let test_image = new Image()
+
+        test_image.onload = function() {
+            console.log('TMDB-Proxy', 'image proxy is working')
+        }
+
+        test_image.onerror = function() {
+            console.log('TMDB-Proxy', 'image error', tmdb_proxy.path_image + ' not responding, using backup proxy')
+
+            tmdb_proxy.path_image = 'lampa.byskaz.ru/tmdb/img/'
+        }
+
+        test_image.src = TMDB.image('t/p/w200/3txl2FUNZCQUnHQPzkuNc17yLIs.jpg')
+    }
+
     TMDB.image = function(url){
         let base  = Utils.protocol() + 'image.tmdb.org/' + url
 
@@ -47,6 +89,8 @@ function init(){
     console.log('TMDB-Proxy','init')
 
     console.log('TMDB-Proxy','enabled', Storage.field('proxy_tmdb'))
+
+    check()
 }
 
 export default {
