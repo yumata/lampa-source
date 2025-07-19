1d140e01ef035cff3ce24438b1842acdb7332031
Alex Androshchuk <androschuk.a@gmail.com>
2025-07-19 13:43:41 +0300

    Add build process #9

--- gulpfile.js
+++ gulpfile.js
@@ -389,7 +389,8 @@ function buildDoc(done){
 
 exports.pack_webos   = series(sync_webos, uglify_task, public_webos, index_webos);
 exports.pack_tizen   = series(sync_tizen, uglify_task, public_tizen, index_tizen);
-exports.pack_github  = series(sync_github, uglify_task, public_github, write_manifest, index_github);
+exports.pack_github  = series(merge, sync_github, uglify_task, public_github, write_manifest, index_github);
+exports.pack_web     = series(merge, plugins, sass_task, lang_task, sync_web, build_web);
 exports.pack_plugins = series(plugins);
 exports.test         = series(test);
 exports.default = parallel(watch, browser_sync);
