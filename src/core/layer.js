879e1afcafe842143c6c6c6aaccb30c38c74b93c
proandreyyy <proandreyyy@gmail.com>
2025-12-26 16:49:57 +0100

    Add new setting into INTERFACE, that always shows left menu on TV.

--- src/core/layer.js
+++ src/core/layer.js
@@ -41,7 +41,8 @@ function init(){
         'card_interfice_cover',
         'advanced_animation',
         'light_version',
-        'keyboard_type'
+        'keyboard_type',
+        'menu_always'
     ]
 
 
@@ -254,6 +255,7 @@ function toggleClasses(){
     $('body').toggleClass('advanced--animation', Storage.field('advanced_animation'))
     $('body').toggleClass('light--version',Storage.field('light_version'))
     $('body').toggleClass('system--keyboard',Storage.field('keyboard_type') == 'lampa' ? false : true)
+    $('body').toggleClass('menu--always', Platform.screen('tv') && Storage.field('menu_always'))
 
     $('body').removeClass('glass--style-opacity--easy glass--style-opacity--medium glass--style-opacity--blacked')
     
