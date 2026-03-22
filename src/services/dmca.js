55e6d2ec55bb5d9e65172a6cd3ec8215f47cd19f
yumata <91452496+yumata@users.noreply.github.com>
2026-03-22 12:38:10 +0200

    Removed cache in DCMA

--- src/services/dmca.js
+++ src/services/dmca.js
@@ -9,16 +9,16 @@ import Arrays from '../utils/arrays'
  */
 function init(){
     if(!window.lampa_settings.disable_features.dmca){
-        Cache.getData('other', 'dcma', 60 * 24 * 10).then((result)=>{
-            if(result && Arrays.isArray(result)) window.lampa_settings.dcma = result
-            else{
+        // Cache.getData('other', 'dcma', 60 * 24 * 10).then((result)=>{
+        //     if(result && Arrays.isArray(result)) window.lampa_settings.dcma = result
+        //     else{
                 Lampa.Network.silent(Utils.protocol() + 'tmdb.'+Manifest.cub_domain+'/blocked',(dcma)=>{
                     window.lampa_settings.dcma = dcma
 
-                    Cache.rewriteData('other', 'dcma', dcma).catch((e)=>{})
+                    //Cache.rewriteData('other', 'dcma', dcma).catch((e)=>{})
                 })
-            }
-        }).catch(e=>{})
+        //     }
+        // }).catch(e=>{})
     }
 }
 
