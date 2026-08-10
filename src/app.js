d94ceeb2461930c9a2dd1a62b089aef927a4505d
yumata <91452496+yumata@users.noreply.github.com>
2026-08-10 19:44:00 +0300

    Remove tester

--- src/app.js
+++ src/app.js
@@ -124,7 +124,6 @@ import ServiceFPS from './services/fps'
 import ServiceEvents from './services/events'
 import ServiceChildren from './services/children'
 import ServiceRemoteConfiguration from './services/remote_configuration'
-import ServiceTester from './services/tester'
 
 window.screen_width  = window.innerWidth
 window.screen_height = window.innerHeight
@@ -728,10 +727,6 @@ function startApp(){
     window.app_time_end = Date.now()
 
     Lampa.Listener.send('app',{type:'ready'})
-
-    // Для тестирования кода
-
-    ServiceTester.init()
 }
 
 /**
